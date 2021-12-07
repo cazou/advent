771e0371b54b626ad0a6d5c72d215657d21a59d5
Detlev Casanova <detlev.casanova@gmail.com>
2021-12-07 10:00:00 -0500

    Add day 7

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day6;
+mod day7;
 
 fn main() -> Result<(), String> {
     match day1::run(include_str!("../inputs/2021-01.txt")) {
@@ -20,6 +22,21 @@ fn main() -> Result<(), String> {
     }
 
     match day4::run(include_str!("../inputs/2021-04.txt")) {
+        Ok(_) => {},
+        Err(e) => return Err(e.to_string())
+    }
+
+    match day6::run(include_str!("../inputs/2021-06.txt")) {
+        Ok(_) => {},
+        Err(e) => return Err(e.to_string())
+    }
+
+    match day6::run2(include_str!("../inputs/2021-06.txt")) {
+        Ok(_) => {},
+        Err(e) => return Err(e.to_string())
+    }
+
+    match day7::run(include_str!("../inputs/2021-07.txt")) {
         Ok(_) => {},
         Err(e) => return Err(e.to_string())
     }
