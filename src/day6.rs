76260cde87aa0384cfa2783298887ba2a6f3a0bb
Detlev Casanova <detlev.casanova@gmail.com>
2022-12-08 14:12:03 -0500

    Make day6 way faster

--- src/day6.rs
+++ src/day6.rs
@@ -1,52 +1,18 @@
-use anyhow::Result;
-use std::str::FromStr;
+use anyhow::{Context, Result};
+use std::collections::HashSet;
 
-struct Marker {
-    message: String,
-}
-
-impl Marker {
-    fn find_marker(&self, len: usize) -> usize {
-        let mut start = 0;
-        let mut end = len;
-        let mut not_it = true;
-        while not_it {
-            not_it = false;
-            for i in start..end {
-                let l = self.message.chars().nth(i).unwrap();
-                for j in i + 1..end {
-                    let r = self.message.chars().nth(j).unwrap();
-                    if l == r {
-                        not_it = true;
-                        break;
-                    }
-                }
-
-                if not_it {
-                    break;
-                }
-            }
-            start += 1;
-            end += 1;
-        }
-
-        end - 1
-    }
-}
-
-impl FromStr for Marker {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Marker {
-            message: s.to_string(),
-        })
-    }
+fn find_marker(input: &str, len: usize) -> Result<usize> {
+    input
+        .as_bytes()
+        .windows(len)
+        .position(|w| w.iter().collect::<HashSet<_>>().len() == len)
+        .map(|p| p + len)
+        .context(format!("Cannot find marker in '{}'", input))
 }
 
 pub fn run(input: &str) -> Result<()> {
-    let marker: Marker = input.parse().unwrap();
-    println!("Packet start: {}", marker.find_marker(4));
-    println!("Message start: {}", marker.find_marker(14));
+    println!("Packet start: {}", find_marker(input, 4)?);
+    println!("Message start: {}", find_marker(input, 14)?);
+
     Ok(())
 }
